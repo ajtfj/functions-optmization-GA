db9a2f37153ce44ee78c5585eb719db8aded5289
Anselmo José Tavares Ferreira Junior <anselmo.jtfj@gmail.com>
2023-08-08 23:56:34 -0300

    Implement arithmetic crossover

--- Optimization.py
+++ Optimization.py
@@ -18,9 +18,25 @@ class Optimization:
 
         return chromosome
 
+    def crossover(self, parent_1: list[float], parent_2: list[float]):
+        if random.uniform(0,1) > self.crossover_rate:
+            return [parent_1, parent_2]
 
-    def crossover(self):
-        raise Exception("Not implemented")
+        crossover_point = random.randint(1, len(parent_1) - 1)
+        a = random.uniform(0,1)
+        offspring_1 = parent_1[:crossover_point] + [
+            a * g1 + (1 - a) * g2 
+            for g1, g2 in zip(parent_1[crossover_point:], parent_2[crossover_point:])
+        ]
+        offspring_2 = parent_2[:crossover_point] + [
+            a * g2 + (1 - a) * g1 
+            for g1, g2 in zip(parent_1[crossover_point:], parent_2[crossover_point:])
+        ]
+
+        offspring_1 = self.mutate(offspring_1)
+        offspring_2 = self.mutate(offspring_2)
+
+        return [offspring_1, offspring_2]
         
     def parents_select(self):
         raise Exception("Not implemented")

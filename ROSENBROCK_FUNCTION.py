533d64638cc9e13404bf35c0acd19a349d31868d
Anselmo José Tavares Ferreira Junior <anselmo.jtfj@gmail.com>
2023-08-09 01:17:46 -0300

    Refactor Rosenbrock function

--- ROSENBROCK_FUNCTION.py
+++ ROSENBROCK_FUNCTION.py
@@ -1,9 +1,2 @@
 def rosenbrock(x: list[float]):
-    d = len(x)
-    sum = 0
-    for i, xi in enumerate(x[:d-1]):
-        first_term = 100 * (x[i+1] - (xi*xi))*(x[i+1] - (xi*xi))
-        second_term = (xi - 1)*(xi - 1)
-        sum += first_term + second_term
-
-    return sum
+    return sum(100 * (x[i + 1] - x[i] ** 2) ** 2 + (1 - x[i]) ** 2 for i in range(len(x) - 1))

f730540814126a3684e91d9f4dc17fa0ae5da9ad
Anselmo José Tavares Ferreira Junior <anselmo.jtfj@gmail.com>
2023-08-09 00:01:06 -0300

    Implement analyses

---
+++ main.py
@@ -0,0 +1,23 @@
+from Optimization import Optimization
+import numpy as np
+
+def find_solution(optimization: Optimization):
+    optimization.generate_population()
+    population_fitness = optimization.rank()
+    solution = optimization.solution()
+    best_fitness_history = [population_fitness[0][1]]
+    average_fitness_history = [np.mean(list(map(lambda x: x[1], population_fitness)))]
+
+    evaluation = 0
+    while solution == None and evaluation < 10000:
+        parents = optimization.parents_select(population_fitness)
+        children = []
+        children.extend(optimization.crossover(parents[0], parents[1]))
+        optimization.survivors_select(children)
+        population_fitness = optimization.rank()
+        solution = optimization.solution()
+        best_fitness_history.append(population_fitness[0][1])
+        average_fitness_history.append(np.mean(list(map(lambda x: x[1], population_fitness))))
+        evaluation += 1
+    
+    return evaluation, best_fitness_history, average_fitness_history
